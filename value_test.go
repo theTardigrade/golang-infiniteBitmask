9a408086faade57ca83089cc19c16aa263412672
James <jismith1989@googlemail.com>
2022-02-09 12:17:05 +0000

    Add Value tests

---
+++ value_test.go
@@ -0,0 +1,53 @@
+package infiniteBitmask
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type testValueDatum struct {
+	suite.Suite
+	Generator *Generator
+	Value     *Value
+}
+
+func (suite *testValueDatum) SetupTest() {
+	suite.Generator = NewGenerator()
+	suite.Value = suite.Generator.newValue(0)
+}
+
+func (suite *testValueDatum) TestString() {
+	assert.Equal(suite.T(), "0", suite.Value.String())
+
+	suite.Value.inner.number.SetUint64(32)
+
+	assert.Equal(suite.T(), "100000", suite.Value.String())
+}
+
+func (suite *testValueDatum) TestClear() {
+	suite.Value.inner.number.SetUint64(1e6)
+	suite.Value.Clear()
+
+	assert.Equal(suite.T(), uint64(0), suite.Value.inner.number.Uint64())
+}
+
+func (suite *testValueDatum) TestIsEmpty() {
+	assert.Equal(suite.T(), true, suite.Value.IsEmpty())
+}
+
+func (suite *testValueDatum) TestIsNotEmpty() {
+	assert.Equal(suite.T(), false, suite.Value.IsNotEmpty())
+}
+
+func (suite *testValueDatum) TestClone() {
+	valueCloned := suite.Value.Clone()
+
+	assert.Equal(suite.T(), true, suite.Value.Equal(valueCloned))
+	assert.Equal(suite.T(), suite.Value.String(), valueCloned.String())
+}
+
+func TestValue(t *testing.T) {
+	suite.Run(t, new(testValueDatum))
+}
